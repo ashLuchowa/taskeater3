ad42f5e0bac21a1c97ead5d075a26c0609de1c37
Ash Luchowa <ashwinluchowa@gmail.com>
2025-04-21 12:35:33 +0800

    Add project list and button + reset CSS

--- src/dom/sideUI.js
+++ src/dom/sideUI.js
@@ -1,1 +1,61 @@
-export const greeting = 'Hello, Ashz';
+import { ManageProject } from "../events/manageProjects";
+
+class SideUI {
+    constructor(outerSelector, sideSelector) {
+        this.outerContainer = document.querySelector(outerSelector);
+        this.sideSelector = sideSelector;
+        this.sideContainer = null;
+    }
+
+    renderSideContainer() {
+        this.sideContainer = document.createElement('div');
+        this.sideContainer.classList.add(this.sideSelector);
+
+        this.outerContainer.appendChild(this.sideContainer);
+    }
+
+    renderLogo(projectTitle) {
+        const logoContainer = document.createElement('div');
+        logoContainer.classList.add('logo-container');
+
+        const logoText = document.createElement('a');
+        logoText.textContent = projectTitle;
+
+        logoContainer.appendChild(logoText);
+
+        this.sideContainer.appendChild(logoContainer);
+    }
+
+    renderAddBtn(type, selectorName, nameContent) {
+        const addBtnContainer = document.createElement(type);
+        addBtnContainer.classList.add(selectorName);
+        addBtnContainer.textContent = nameContent;
+
+        this.sideContainer.appendChild(addBtnContainer);
+    }
+
+    renderProjectList() {
+        // prevent duplication
+        const existingContainer = document.querySelector('.project-list-container');
+        if (existingContainer) {
+            existingContainer.remove();
+        }
+
+        const projectListContainer = document.createElement('div');
+        projectListContainer.classList.add('project-list-container');
+        const projectItemMain = document.createElement('ul');
+
+        ManageProject.projects.forEach(itemProject => {
+            const projectItem = document.createElement('li');
+            projectItem.classList.add('project-list-item');
+            projectItem.textContent = itemProject.title;
+
+            projectItemMain.appendChild(projectItem);
+        });
+
+        projectListContainer.appendChild(projectItemMain);
+        this.sideContainer.appendChild(projectListContainer);
+    }
+}
+
+export const generateSideUI = new SideUI('.content', 'side-container');
--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@ import "./styles.css";
 import { greeting } from "./dom/sideUI";
 import { ManageProject } from "./events/manageProjects";
 import { ManageTask } from "./events/manageTasks";
+import { generateSideUI } from "./dom/sideUI";
 
 
 function initialiseApp() {
+    // Push default projects and tasks into related arrays
     const result = ManageProject.projects;
     ManageProject.pushDefaultProjects();
     ManageProject.pushNewProjects();
-    ManageProject.pushNewProjects();
     ManageTask.pushTasks(ManageTask.defaultTasks);
     console.log(result);
+
+    // Init Sidebar
+    generateSideUI.renderSideContainer();
+    generateSideUI.renderLogo('Logo');
+    generateSideUI.renderAddBtn('button', 'add-project-btn', 'Add Project');
+    generateSideUI.renderProjectList();
 }
 
 initialiseApp();
